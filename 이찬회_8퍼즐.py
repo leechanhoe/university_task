4b2c2f5a1c7bc5c53e98025cd16f75f504217f07
Chan <54972898+leechanhoe@users.noreply.github.com>
2022-09-21 08:16:25 +0900

    Create repository

---
+++ 이찬회_8퍼즐.py
@@ -0,0 +1,100 @@
+from collections import deque
+import sys
+import heapq
+from copy import deepcopy
+
+
+def bfs():  # 너비 우선 탐색 - 완전탐색이므로 최적해를 구한다
+    print("목표 상태를 입력하세요.\n예시\n1 2 3\n4 5 6\n7 8 0")
+    goal = []
+    for i in range(3):
+        goal += input().split()
+
+    print("처음 상태를 입력하세요.")
+    board = []
+    for i in range(3):
+        board += input().split()
+
+    dq = deque()
+    dq.append([board, board.index("0"), 0])
+    visit = {}
+    visit["".join(board)] = True
+
+    while dq:
+        now, blank, cnt = dq.popleft()
+        if now == goal:
+            print(f"최소 이동 횟수 : {cnt}")
+            exit(0)
+
+        for dir in [1, -1, 3, -3]:  # 우, 좌, 하, 상
+            nBlank = dir + blank
+
+            if 0 <= nBlank < 9:
+                next = deepcopy(now)
+                next[blank], next[nBlank] = next[nBlank], next[blank]
+
+                conv = "".join(next)  # 방문체크를 하기위해 현재 상태를 문자열로 변환
+                if conv not in visit:  # 공간복잡도를 줄이기 위해 딕셔너리로 방문체크
+                    visit[conv] = True
+                    dq.append([next, nBlank, cnt + 1])
+    print("도달할 수 없는 목표입니다.")
+
+
+def h(board, goal):  # 휴리스틱 함수 -> 목표와 다른 칸의 개수를 반환
+    ret = 0
+    for i in range(9):
+        if(board[i] != goal[i]):
+            ret += 1
+    return ret
+
+
+def dfs(visit, now, goal, blank, cnt):  # 깊이 우선 탐색 dfs(방문체크, 현재상태, 목표상태, 0의 위치, 현재까지의 비용)
+    if now == goal:
+        print(f"최소 이동 횟수 : {cnt}")
+        exit(0)
+    hq = []
+    for dir in [1, -1, 3, -3]:
+        nBlank = blank + dir
+
+        if 0 <= nBlank < 9:
+            next = deepcopy(now)
+            next[blank], next[nBlank] = next[nBlank], next[blank]
+
+            convert = "".join(next)  # 방문체크를 하기위해 현재 상태를 문자열로 변환
+            if convert not in visit:  # 공간복잡도를 줄이기 위해 딕셔너리로 방문체크
+                visit[convert] = True
+                # 우선순위 큐(최소힙)를 활용해 f(n)의 최솟값부터 탐색
+                # cnt + h(next, goal) = g(n) + h(n)
+                heapq.heappush(hq, (cnt + h(next, goal), next, nBlank))
+
+    while hq:
+        fn, next, nBlank = heapq.heappop(hq)  # 작은 값부터 꺼내 dfs
+        dfs(visit, next, goal, nBlank, cnt + 1)
+
+
+def aStar():  # A* 알고리즘
+    sys.setrecursionlimit(10 ** 7)
+    print("목표 상태를 입력하세요.\n예시\n1 2 3\n4 5 6\n7 8 0")
+    goal = []
+    for i in range(3):
+        goal += input().split()
+
+    print("처음 상태를 입력하세요.")
+    board = []
+    for i in range(3):
+        board += input().split()
+
+    visit = {}  # 방문체크
+    visit["".join(board)] = True  # 공간복잡도를 줄이기 위해 문자열로 변환 후 딕셔너리에 저장
+
+    dfs(visit, board, goal, board.index("0"), 0)
+    print("도달할 수 없는 목표입니다.")
+
+
+print("최적해를 구하는 BFS알고리즘 : 0입력\n 근사해를 구하는 A*알고리즘 : 1입력")
+mode = int(input())
+
+if(mode == 0):
+    bfs()
+else:
+    aStar()
---
+++ 이찬회_틱택토.py
@@ -0,0 +1,109 @@
+def go(turn):
+    # 만약 현재 맵 상태에서 상대턴이 이겼다면 더 수를 둘 필요가 없다.
+    if test(3 - turn):  # 3 - turn은 상대방을 의미한다
+        return [0, 1, 0]
+
+    result = [0, 0, 0]  # [비김, 이김, 짐, 총 탐색 횟수]
+    for y in range(3):
+        for x in range(3):
+            if board[y][x] == 0:
+                board[y][x] = turn
+                nextResult = go(3 - turn)  # dfs
+
+                for i in range(3):  # 빈칸을 모두 탐색하여 각 횟수들을 더함
+                    result[i] += nextResult[i]
+                board[y][x] = 0
+                result[2] += 1
+
+    # 여기가 중요. 이긴 횟수와 진 횟수를 바꿔서 반환 - Min Max Algorithm
+    return [result[1], result[0], result[2]]
+
+
+def test(turn):  # 결과가 나왔는지 체크
+    for y in range(3):
+        if board[y][0] == turn and board[y][0] == board[y][1] and board[y][1] == board[y][2]:
+            return True
+
+    for x in range(3):
+        if board[0][x] == turn and board[0][x] == board[1][x] and board[1][x] == board[2][x]:
+            return True
+
+    if board[0][0] == turn and board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        return True
+
+    if board[0][2] == turn and board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        return True
+
+    return False
+
+
+def computerTurn(board):
+    order = 0
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] != 0:
+                order += 1
+    turn = 1 if order % 2 == 0 else 2
+
+    optimum = [-1, -1]  # 승률이 가장 높은 자리
+    maxWinRate = -1
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                board[i][j] = turn
+                result = go(turn)  # [이긴 횟수, 진 횟수, 총 탐색 횟수]를 반환
+                winRate = (result[0] / result[2]) - (result[1] / result[2])
+                if maxWinRate < winRate:  # 이길확률 - 질 확률이 제일 높은 곳
+                    maxWinRate = winRate
+                    optimum = [i, j]
+                board[i][j] = 0
+
+    board[optimum[0]][optimum[1]] = turn  # 최적의 장소에 착수
+
+
+def userTurn(board):
+    while 1:
+        print("행과 열을 입력하세요. 예)2 3    게임종료는 -1, -1")
+        try:
+            y, x = map(int, input().split())
+        except:
+            print("빈칸이 아니거나 잘못된 입력입니다.")
+            continue
+        if y == -1 and x == -1:
+            exit(0)
+
+        if board[y-1][x-1] == 0:
+            board[y-1][x-1] = 1
+            return
+
+        print("빈칸이 아니거나 잘못된 입력입니다.")
+
+
+def printBoard():
+    print(" |1|2|3|")
+    print("--------")
+    for i in range(3):
+        print(str(i + 1) + "|", end="")
+        for j in range(3):
+            if board[i][j] == 1:
+                print("O|", end='')
+            elif board[i][j] == 2:
+                print("X|", end='')
+            else:
+                print(" |", end='')
+        print("\n--------")
+
+
+board = [[0] * 3 for _ in range(3)]  # 사용자는 1 컴퓨터는 2로 저장
+print("사용자가 선공입니다. 사용자 : O / 컴퓨터 X")
+for i in range(9):
+    userTurn(board) if i % 2 == 0 else computerTurn(board)
+    printBoard()
+
+    if test(1):
+        print("사용자가 이겼습니다.")
+        exit(0)
+    if test(2):
+        print("컴퓨터가 이겼습니다.")
+        exit(0)
+print("비겼습니다.")
